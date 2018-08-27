0eedac8850ff36e94d352147f832dffb06d8e38e
Alex <agilliard@gmail.com>
2018-08-27 15:41:40 -0400

    Now Printing out title of Image.

--- main.py
+++ main.py
@@ -7,25 +7,31 @@ import urllib.request
 print('Downloading all saved Reddit Images...\n')
 
 reddit = praw.Reddit(client_id=cfg.client_id,
-                     client_secret=cfg.client_secret,
-                     password=cfg.password,
-                     user_agent=cfg.user_agent,
-                     username=cfg.username)
+					client_secret=cfg.client_secret,
+					password=cfg.password,
+					user_agent=cfg.user_agent,
+					username=cfg.username)
 
 saved_posts = reddit.user.me().saved(limit=None)
 
 #filter out only images and gifs
-links =[]
+links = []
+title = []
 for link in saved_posts:
 	if link.url[-3:].upper() in ['JPG','PNG','GIF']:
+		title.append(link.title)
 		links.append(link.url)
 
+print(f'{len(links)} images will be downloaded...\n')
 
+#download files
+index = 0
 for l in links:
 	url = l
-	print('Downloading: '+url)
+	print(f'Downloading: {title[index]}  ({url})')
 	file_name = re.search(r'(?=\w+\.\w{3,4}$).+',url).group(0)
 	urllib.request.urlretrieve(url,'saved/'+file_name)
 	time.sleep(2)
+	index+=1
 
 print('\nProcess Complete!')
